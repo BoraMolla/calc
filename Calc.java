9dbb353e3663723c938d86e3d61eddf8006627da
xu-ji <xj1112@ic.ac.uk>
2012-11-25 18:29:39 +0000

    br - added greeting

--- Calc.java
+++ Calc.java
@@ -5,6 +5,7 @@ import java.lang.*;
 public class Calc {
 
   public static void main(String[] args) {
+    System.out.println("Welcome to Calc!");
     System.out.println("Enter expression:");
     String expr = IOUtil.readString();
 
