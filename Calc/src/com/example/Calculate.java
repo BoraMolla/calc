8245c68853dda842aaf21311c4995ea6348076d3
GDS12 <gds12@imperial.ac.uk>
2012-11-26 18:18:48 +0000

    Seperated Calculate and MainActivity Files.

---
+++ Calc/src/com/example/Calculate.java
@@ -0,0 +1,60 @@
+package com.example;
+
+import java.util.Hashtable;
+import java.util.Stack;
+
+public class Calculate {
+	public static String calculateExpr(String expr){
+		   Stack<Integer> exprStack = new Stack<Integer>();
+		   Stack<Character> opStack = new Stack<Character>();
+		   opStack.push('$');
+		   String val = "0";
+		   
+		   Hashtable<Character,Integer> opTable = new Hashtable<Character,Integer>();
+		   opTable.put('+', 1);
+		   opTable.put('-', 1);
+		   opTable.put('*', 2);
+		   opTable.put('/', 2);
+		   opTable.put('$', 0);
+		   for (int i=0; i<expr.length(); i++) {
+		       if (isNum(expr.charAt(i))) {
+		         val += expr.charAt(i);
+		       } else {
+		           if (!val.equals("0")){
+		             exprStack.push(Integer.parseInt(val));
+		           }
+		           val = "0";
+		           if ((opTable.get(expr.charAt(i))).hashCode()>(opTable.get(opStack.peek())).hashCode()){
+		             opStack.push(expr.charAt(i));
+		           }else {
+		             eval(exprStack, opStack);
+		             i--;
+		           }
+		       }
+		     if (i==expr.length()-1)   
+		       exprStack.push(Integer.parseInt(val));
+		   }
+		
+		   while (exprStack.size()!=1) {
+		     eval(exprStack, opStack);
+		   }
+		
+		   return exprStack.pop().toString();
+		 }
+		
+		 private static void eval(Stack<Integer> exprStack, Stack<Character> opStack) {
+		   int ex2 = exprStack.pop().hashCode();
+		   int ex1 = exprStack.pop().hashCode();
+		   char op = (char)opStack.pop().hashCode();
+		   switch (op) {
+		     case '+': exprStack.push(ex1+ex2); break;
+		     case '-': exprStack.push(ex1-ex2); break;
+		     case '*': exprStack.push(ex1*ex2); break;
+		     case '/': exprStack.push(ex1/ex2); break;
+		     default: System.out.println("Failure"); break;
+		   }
+		 }
+		 private static boolean isNum(char x) {
+		   return ('0' <= x && x <= '9');
+		 } //*/
+}
--- Calc/src/com/example/MainActivity.java
+++ Calc/src/com/example/MainActivity.java
@@ -1,8 +1,5 @@
 package com.example;
 
-import java.util.Hashtable;
-import java.util.Stack;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -28,63 +25,8 @@ public class MainActivity extends Activity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String expr = ((EditText) findViewById(R.id.exprEntry)).getText().toString();
-                ((TextView) findViewById(R.id.resultText)).setText(calculateExpr(expr));
+                ((TextView) findViewById(R.id.resultText)).setText(Calculate.calculateExpr(expr));
             }
         });
     }
-	
-	
-	private static String calculateExpr(String expr){
-	   Stack<Integer> exprStack = new Stack<Integer>();
-	   Stack<Character> opStack = new Stack<Character>();
-	   opStack.push('$');
-	   String val = "0";
-	   
-	   Hashtable<Character,Integer> opTable = new Hashtable<Character,Integer>();
-	   opTable.put('+', 1);
-	   opTable.put('-', 1);
-	   opTable.put('*', 2);
-	   opTable.put('/', 2);
-	   opTable.put('$', 0);
-	   for (int i=0; i<expr.length(); i++) {
-	       if (isNum(expr.charAt(i))) {
-	         val += expr.charAt(i);
-	       } else {
-	           if (!val.equals("0")){
-	             exprStack.push(Integer.parseInt(val));
-	           }
-	           val = "0";
-	           if ((opTable.get(expr.charAt(i))).hashCode()>(opTable.get(opStack.peek())).hashCode()){
-	             opStack.push(expr.charAt(i));
-	           }else {
-	             eval(exprStack, opStack);
-	             i--;
-	           }
-	       }
-	     if (i==expr.length()-1)   
-	       exprStack.push(Integer.parseInt(val));
-	   }
-	
-	   while (exprStack.size()!=1) {
-	     eval(exprStack, opStack);
-	   }
-	
-	   return exprStack.pop().toString();
-	 }
-	
-	 private static void eval(Stack<Integer> exprStack, Stack<Character> opStack) {
-	   int ex2 = exprStack.pop().hashCode();
-	   int ex1 = exprStack.pop().hashCode();
-	   char op = (char)opStack.pop().hashCode();
-	   switch (op) {
-	     case '+': exprStack.push(ex1+ex2); break;
-	     case '-': exprStack.push(ex1-ex2); break;
-	     case '*': exprStack.push(ex1*ex2); break;
-	     case '/': exprStack.push(ex1/ex2); break;
-	     default: System.out.println("Failure"); break;
-	   }
-	 }
-	 private static boolean isNum(char x) {
-	   return ('0' <= x && x <= '9');
-	 } //*/
 }
