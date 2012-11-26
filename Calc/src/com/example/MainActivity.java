7a1e5e2d943a31e1de9c1167a07a58b11c412b62
GDS12 <gds12@imperial.ac.uk>
2012-11-26 17:47:30 +0000

    Fixed a lot of everything and still doesn't work :(

--- Calc/src/com/example/MainActivity.java
+++ Calc/src/com/example/MainActivity.java
@@ -1,20 +1,91 @@
 package com.example;
 
+import java.util.Hashtable;
+import java.util.Stack;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String expr = v.toString();
+                ((TextView) findViewById(R.id.resultText)).setText(calculateExpr(expr));
+            }
+        });
+    }
+	
+	
+	private static String calculateExpr(String expr){
+	   Stack<Integer> exprStack = new Stack<Integer>();
+	   Stack<Character> opStack = new Stack<Character>();
+	   opStack.push('$');
+	   String val = "0";
+	
+	   Hashtable<Character,Integer> opTable = new Hashtable<Character,Integer>();
+	   opTable.put('+', 1);
+	   opTable.put('-', 1);
+	   opTable.put('*', 2);
+	   opTable.put('/', 2);
+	   opTable.put('$', 0);
+	   for (int i=0; i<expr.length(); i++) {
+	       if (isNum(expr.charAt(i))) {
+	         val += expr.charAt(i);
+	       } else {
+	           if (!val.equals("0"))
+	             exprStack.push(Integer.parseInt(val));
+	           val = "0";
+	           if ((opTable.get(expr.charAt(i))).hashCode()>(opTable.get(opStack.peek())).hashCode())
+	             opStack.push(expr.charAt(i));
+	             
+	           else {
+	             eval(exprStack, opStack);
+	             i--;
+	           }
+	       }
+	     if (i==expr.length()-1)   
+	       exprStack.push(Integer.parseInt(val));
+	   }
+	
+	   while (exprStack.size()!=1) {
+	     eval(exprStack, opStack);
+	   }
+	
+	   return exprStack.pop().toString();
+	
+	 }
+	
+	 private static void eval(Stack<Integer> exprStack, Stack<Character> opStack) {
+	   int ex2 = exprStack.pop().hashCode();
+	   int ex1 = exprStack.pop().hashCode();
+	   char op = (char)opStack.pop().hashCode();
+	   switch (op) {
+	     case '+': exprStack.push(ex1+ex2); return;
+	     case '-': exprStack.push(ex1-ex2); return;
+	     case '*': exprStack.push(ex1*ex2); return;
+	     case '/': exprStack.push(ex1/ex2); return;
+	     default: System.out.println("Failure"); return;
+	   }
+	 }
+	
+	 private static boolean isNum(char x) {
+	   return ('0' <= x && x <= '9');//*/
+	 } 
 }
